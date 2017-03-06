2ed1fd4582d756bcc2db37b4bb1145b6f4bee879
Kayhen <hugoduarte.dev@gmail.com>
2017-03-06 13:50:10 +0000

    Added timeline animation to the timer.

--- src/com/teamtreehouse/pomodoro/controllers/Home.java
+++ src/com/teamtreehouse/pomodoro/controllers/Home.java
@@ -1,12 +1,16 @@
 package com.teamtreehouse.pomodoro.controllers;
 
-
 import com.teamtreehouse.pomodoro.model.Attempt;
 import com.teamtreehouse.pomodoro.model.AttemptKind;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 
 public class Home {
@@ -17,12 +21,53 @@ public class Home {
     private Label title;
 
     private Attempt currentAttempt;
+    private StringProperty timerText;
+    private Timeline timeline;
+
+    public Home() {
+        this.timerText = new SimpleStringProperty();
+        setTimerText(0);
+    }
+
+    public String getTimerText() {
+        return this.timerText.get();
+    }
+
+    public StringProperty timerTextProperty() {
+        return this.timerText;
+    }
+
+    public void setTimerText(String timerText) {
+        this.timerText.set(timerText);
+    }
+
+    public void setTimerText(int remainingSeconds) {
+        int minutes = remainingSeconds / 60;
+        int seconds = remainingSeconds % 60;
+        setTimerText(String.format("%02d:%02d", minutes, seconds));
+    }
 
     private void prepareAttempt(AttemptKind kind) {
         clearAttemptStyles();
         this.currentAttempt = new Attempt(kind, "");
         addAttemptStyle(kind);
         title.setText(kind.getDisplayName());
+        setTimerText(this.currentAttempt.getRemainingSeconds());
+        this.timeline = new Timeline();
+        // TODO: This is creating multiple times, needs fixing
+        this.timeline.setCycleCount(kind.getTotalSeconds());
+        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(1), e -> {
+            currentAttempt.tick();
+            setTimerText(currentAttempt.getRemainingSeconds());
+        }));
+    }
+
+    public void playTimer() {
+        timeline.play();
+    }
+
+    public void pauseTimer() {
+        timeline.pause();
     }
 
     private void addAttemptStyle(AttemptKind kind) {
@@ -36,6 +81,11 @@ public class Home {
     }
 
     public void DEBUG(ActionEvent actionEvent) {
+        prepareAttempt(AttemptKind.BREAK);
+    }
+
+    public void handleRestart(ActionEvent actionEvent) {
         prepareAttempt(AttemptKind.FOCUS);
+        playTimer();
     }
 }
